af896d5e5f6e831402eec5196f05d01013d205a9
shubhamcompro23 <shubham.saini@comprotechnologies.com>
2023-05-01 17:21:56 +0530

    set , sorted set

--- redis/sets.js
+++ redis/sets.js
@@ -5,7 +5,25 @@ client.connect();
 client.on('connect', () => console.log('connect to redis server'));
 
 (async() =>{
-    await client.SADD("mylist", ["IPHONE", "laptop"]);
-    console.log("smembers",await client.SMEMBERS('mylist'))
-    console.log("ismembers",await client.SISMEMBER('mylist',"laptop"))
+    await client.SADD("phones", ["iphone", "samsung", "redme"]);
+    await client.SADD("laptop", ["dell", "asus", "hp", "macbook"]);
+    await client.SPOP("laptop");
+    console.log("smembers",await client.SMEMBERS('phones'))
+    console.log("ismembers",await client.SISMEMBER('phones',"redme"))
+    console.log("Total member in a set",await client.SCARD('laptop',))
+    console.log("uniont",await client.SUNION(['laptop',"phones"]))
+
+
+
+
+    await client.SADD("user:1:favorites", "55");
+    await client.SADD("user:2:favorites", "56");
+    await client.SADD("user:2:favorites", "55");
+    await client.SADD("user:1:favorites", "56");
+    await client.SADD("user:1:favorites", "20");
+
+    console.log("sInter",await client.SINTER(["user:1:favorites","user:2:favorites"]))
+    console.log("remove",await client.SREM("user:1:favorites", "20"))
+
+
 })()
--- redis/sortedSets.js
+++ redis/sortedSets.js
@@ -5,7 +5,34 @@ client.connect();
 client.on('connect', () => console.log('connect to redis server'));
 
 (async() =>{
-    // await client.set("username", "shubham saini");
 
-    // console.log("getrange---", await client.GETRANGE("username", "0", "7"))
+    console.log(
+        await client.zAdd('scoreboard', [{
+          score: 10,
+          value: 'user:1'
+        }, {
+          score: 2,
+          value: 'user:2'
+        },{
+            score: 55,
+            value: 'user:3'
+        },{
+            score: 65,
+            value: 'user:4'
+        }])
+      );
+
+    console.log(await client.ZRANGE("scoreboard",0,100,["SCORE", "REV"]))
+
+    console.log("Total member in a set",await client.ZCARD('scoreboard',))
+    console.log("member in a set b/w range",await client.ZCOUNT('scoreboard',50,100))
+    console.log("increby",await client.ZINCRBY('scoreboard',10,"user:2"))
+    console.log("score--",await client.ZSCORE('scoreboard',"user:1"))
+    console.log("ZRANK",await client.ZRANK('scoreboard',"user:3"))
+
+
+    //console.log("zInter",await client.SINTER(["scoreboard", "livescore"]))
+    console.log("remove",await client.ZREM("scoreboard","user:1"))
+
+
 })()
